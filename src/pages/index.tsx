b1071c62af2f2ac6d051d9fe4f7de20e5cc2cd98
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2021-12-13 21:07:11 +0900

    色々追加

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <p className="text-3xl font-bold underline hover:text-red-300">
+        テキスト
+      </p>
+    </div>
+  );
+};
+
+export default Home;
